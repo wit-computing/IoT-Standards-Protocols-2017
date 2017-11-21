b7c890a20d87062fbc8e96b1b6e050bc2310b261
fxwalsh <fxwalsh@wit.ie>
2017-11-21 10:55:55 +0000

    new topic dfor AWS

--- topic03/book-1/code/main.py
+++ topic03/book-1/code/main.py
@@ -1,9 +1,18 @@
 from pysense import Pysense
+
+from SI7006A20 import SI7006A20
+
+from LTR329ALS01 import LTR329ALS01
+
 import time
 from network import Sigfox
 import socket
 
 py = Pysense()
+
+si = SI7006A20(py)
+
+lt = LTR329ALS01(py)
 
 
 # init Sigfox for RCZ1 (Europe)
@@ -20,9 +29,9 @@ s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
 while True:
     #temperature = int(round(si.temperature()*100))
-    temperature = 24
-    light = 350
+    temperature = int(round(si.temperature()*100))
+    light = lt.light()[0]
     print(str(temperature) + ":"+str(light))
     messageBytes=bytes((temperature & 0xff, ((temperature >> 8) & 0xff),light & 0xff, ((light >> 8) & 0xff)))
-    #s.send(messageBytes)
+    s.send(messageBytes)
     time.sleep(30)
